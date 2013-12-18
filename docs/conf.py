f6d8043fd1a274a8b9a92ed02d35beee7d8cd7c9
Daisuke Igarashi <daisuke@dkpyn.com>
2013-12-19 01:30:01 +0900

    readthedocs用に更新

--- docs/conf.py
+++ docs/conf.py
@@ -234,8 +234,11 @@ blockdiag_html_image_format = 'PNG' # 'PNG' or 'SVG'
 blockdiag_tex_image_format = 'PNG' # 'PNG' or 'SVG'
 
 # -- sphinx_rtd_theme 
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
