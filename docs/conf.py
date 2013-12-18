e6d3e0484c1a454d96743c446d781ead9c5d4cd6
Daisuke Igarashi <daisuke@dkpyn.com>
2013-12-19 01:55:25 +0900

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -242,3 +242,4 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme = 'sphinx_rtd_theme'
